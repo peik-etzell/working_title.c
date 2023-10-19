1157b371c4ace14bf5e71933369ec9ce14e23dae
Peik Etzell <peik.etzell@gmail.com>
2023-10-19 13:27:06 +0300

    Add floor to demo

--- src/demos/cube.c
+++ src/demos/cube.c
@@ -4,23 +4,35 @@
 #include <unistd.h>
 
 #include "../linalg/triangle.h"
+#include "../rendering/primitives/rectangle.h"
 #include "../rendering/render.h"
 
 int main(void) {
     tribuf T = {0};
 
-    object cube = new_cube((tfm){identity(), (vec){0, 0, 50}}, 15);
+    object cube = new_cube((tfm){identity(), (vec){0, 0, 150}}, 20);
     append_cube(&T, &cube);
     mat rot = from_euler(0.01f, 0.02f, 0);
     vec vel = {0, 0, 0};
 
+    object floor = new_rectangle(
+        (tfm){from_euler(-(float)M_PI_2, 0, 0), (vec){0, 50, 150}}, 300, 400
+    );
+    append_rectangle(&T, &floor);
+
     while (1) {
+        if (cube.transform.translation.y < 50) {
+            vel.y += 0.01f;
+        } else {
+            vel.y *= -1;
+        }
         translate_obj(&cube, vel);
         rotate_obj(&cube, rot);
         update(cube);
+        update(floor);
         render(T);
         fflush(stdout);
-        usleep((size_t)(1e6 * 0.01));
+        usleep((size_t)(1e6 * 0.05));
     }
 
     free(cube.vertices);
