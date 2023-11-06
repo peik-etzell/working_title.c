fe35febf5f6ff366d76c005d9293a823ebf3fe7c
Peik Etzell <peik.etzell@gmail.com>
2023-11-06 16:11:18 +0200

    Change demo, partly due to FOV fix

--- src/demos/cube.c
+++ src/demos/cube.c
@@ -9,27 +9,31 @@
 
 int main(void) {
     tribuf T = {0};
-
-    object cube = new_cube((tfm){identity(), (vec){0, 0, 150}}, 20);
+    object cube = new_cube((tfm){identity(), (vec){0, 0, 3}}, 0.1);
     append_cube(&T, &cube);
     mat rot = from_euler(0.01f, 0.02f, 0);
     vec vel = {0, 0, 0};
 
-    object floor = new_rectangle(
-        (tfm){from_euler(-(float)M_PI_2, 0, 0), (vec){0, 50, 150}}, 300, 400
-    );
-    append_rectangle(&T, &floor);
+    // object floor = new_rectangle(
+    //     (tfm){from_euler(-(float)M_PI_2, 0, 0), (vec){0, 50, 50}}, 300, 400
+    // );
+    // append_rectangle(&T, &floor);
 
     while (1) {
-        if (cube.transform.translation.y < 50) {
-            vel.y += 0.01f;
+        if (cube.transform.translation.y < 10) {
+            // vel.y += 0.03f;
         } else {
-            vel.y *= -1;
+            // vel.y *= -1;
+        }
+        if (cube.transform.translation.z > 0) {
+            vel.z -= 0.003f;
+        } else {
+            vel.z *= -1;
         }
         translate_obj(&cube, vel);
         rotate_obj(&cube, rot);
         update(cube);
-        update(floor);
+        // update(floor);
         render(T);
         fflush(stdout);
         usleep((size_t)(1e6 * 0.05));
