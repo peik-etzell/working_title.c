cfe8e6ba368eb80934aabc20240f4f14eecd4ddc
Peik Etzell <peik.etzell@gmail.com>
2023-11-12 02:15:37 +0200

    reallocarray -> realloc because android bug i guess

--- src/utils/buf.h
+++ src/utils/buf.h
@@ -1,20 +1,22 @@
 #ifndef UTILS_BUF_H
 #define UTILS_BUF_H
 
+#include <stdlib.h>
+
 #include "macros.h"
 
-#define DECL_BUF(NAME, TYPE)                                                   \
-    typedef struct {                                                           \
-        TYPE* data;                                                            \
-        size_t n;                                                              \
-        size_t cap;                                                            \
-    } NAME;                                                                    \
-    static void NAME##_append(NAME* buf, TYPE elem) {                          \
-        if (buf->n >= buf->cap) {                                              \
-            buf->cap = max(8UL, buf->cap * 2);                                 \
-            buf->data = reallocarray(buf->data, buf->cap, sizeof(*buf->data)); \
-        }                                                                      \
-        buf->data[buf->n++] = elem;                                            \
+#define DECL_BUF(NAME, TYPE)                                         \
+    typedef struct {                                                 \
+        TYPE* data;                                                  \
+        size_t n;                                                    \
+        size_t cap;                                                  \
+    } NAME;                                                          \
+    static void NAME##_append(NAME* buf, TYPE elem) {                \
+        if (buf->n >= buf->cap) {                                    \
+            buf->cap = max(8UL, buf->cap * 2);                       \
+            buf->data = realloc(buf->data, buf->cap * sizeof(TYPE)); \
+        }                                                            \
+        buf->data[buf->n++] = elem;                                  \
     }
 
 DECL_BUF(intbuf, int);
